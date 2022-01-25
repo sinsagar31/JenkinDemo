6a36cfcaba6eb50b273513b58d109c23605e5ad9
Sagar Singhi <singhisagar088@gmail.com>
2022-01-25 09:48:24 +0530

    Changes test

--- pharma-eshot-service/src/main/java/com/pharma/targeting/eshot/PharmaEshotServiceApplication.java
+++ pharma-eshot-service/src/main/java/com/pharma/targeting/eshot/PharmaEshotServiceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PharmaEshotServiceApplication {
 
 	public static void main(String[] args) {
-		System.out.println("PharmaEshotServiceApplication started......");
+		System.out.println("PharmaEshotServiceApplication started..........");
 		SpringApplication.run(PharmaEshotServiceApplication.class, args);
 		
 	}
